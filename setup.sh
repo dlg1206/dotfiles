8dc5fc01b01b220378098cda231f350765813545
Derek Garcia <dgarcia_81@yahoo.com>
2024-07-19 23:21:37 -0400

    feat: detect os type

--- setup.sh
+++ setup.sh
@@ -5,19 +5,41 @@
 #
 # @author: Derek Garcia
     
-# Define env variables
-export DOTFILE_REPO=https://github.com/dlg1206/dotfiles
 
+# supported configs
 DOTFILES=('gitconfig')
 
+# Iterate through configs and set them up
+#
+# return: 0 if OS is known, 1 otherwise
+__set_os(){
+  case "$OSTYPE" in
+    solaris*) export OS="SOLARIS" ;;
+    darwin*)  export OS="OSX" ;;
+    linux*)   export OS="LINUX" ;;
+    bsd*)     export OS="BSD" ;;
+    msys*)    export OS="WINDOWS" ;;
+    *)
+      echo "Unknown OS '$OSTYPE'" >&2
+      exit 1
+      ;;
+  esac
+  return 0
+}
 
 
+# Iterate through configs and set them up
+#
+# @param $@: array of configs to setup
 main(){
+  # set os, err if unknown
+  __set_os || exit 1
 
   # use args if given, default all
-  if [ -z "$@" ]; then
+  if [ -z "$*" ]; then
     dotfiles=("${DOTFILES[@]}")
   else
+    # todo rm dup
     dotfiles=("$@")
   fi
 
@@ -27,15 +49,16 @@ main(){
     case $config in
       # git
       gitconfig)
-        echo "config"
+        . git/setup.sh
         ;;
 
       # unknown config
       *)
-        echo "warn"
+        echo "Unknown config '$config'" >&2
         ;;
     esac
   done
+  exit 0
 }
 
 
