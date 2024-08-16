48a9ddbfee76bb103aa2514a037e97c9e6c270e4
Derek Garcia <dcia811@gmail.com>
2024-08-15 22:09:30 -1000

    feat: add container for testing git commands

---
+++ gitconfig/dev-env.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# file: dev-env.sh
+# description: build local test environment for testing git alias opterations
+#
+# @author Derek Garcia
+ENV_NAME=git-test
+
+# build image if dne
+docker image ls | grep -qE "$ENV_NAME "
+[ "$?" -eq 1 ] && docker build -t "$ENV_NAME" .
+
+docker run --rm -it "$ENV_NAME"
